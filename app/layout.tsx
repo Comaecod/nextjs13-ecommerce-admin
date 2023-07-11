820c3694c69a8ed4584a0caebd74cd4324b30030
comaecod <vishnuthecoder@gmail.com>
2023-07-11 21:03:54 +0530

    Add Form Components

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,7 +20,9 @@ export default function RootLayout({
   return (
     <ClerkProvider>
       <html lang='en'>
-        <body className={inter.className}>
+        <body
+          className={inter.className}
+          suppressHydrationWarning={true}>
           <ModalProvider />
           {children}
         </body>
