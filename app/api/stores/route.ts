1a76fe1d197e930b9213d1a7da647fd1a623fce1
comaecod <vishnuthecoder@gmail.com>
2023-07-14 18:43:40 +0530

    Add Dashboard Setup

--- app/api/stores/route.ts
+++ app/api/stores/route.ts
@@ -5,9 +5,7 @@ import { NextResponse } from 'next/server';
 export async function POST(req: Request) {
   try {
     const { userId } = auth();
-    const body = await req.json();
-
-    const { name } = body;
+    const { name } = await req.json();
 
     if (!userId) {
       return new NextResponse('Unauthorized', { status: 401 });
